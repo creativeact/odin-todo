6a6836e9ff05c83abfddde841f69ca40d7a16e20
creativeact <128939729+creativeact@users.noreply.github.com>
2024-10-11 17:23:10 -0600

    Adding styling

--- src/index.js
+++ src/index.js
@@ -1,28 +1,86 @@
 import "./style.css";
-import { Task } from "./task.js";
+import { addTask } from "./task.js";
+import { renderProjectPage, hideSidebar } from "./dom.js";
+import { addProjectHeader } from "./dom_project.js";
+import { createTaskCard, removeTaskCard, updateFormProjectOptions } from "./dom_task.js";
+import { getProjects, getTasks, storageAvailable, populateProjectStorage, populateTaskStorage } from './storage.js';
+import { myTasks, myProjects, Task} from './data.js';
 
-// Project Properties
-    // title
-    // percentage complete (as percentage of to dos)
-    // linked to - dos
+storageAvailable('localStorage') ? console.log('Local storage available') : console.log('Local storage not available');
+
+if (!localStorage.getItem('myTasks')) {
+    console.log("Initial populating Task Storage");
+    populateTaskStorage(myTasks, 'myTasks');
+}
+
+if(!localStorage.getItem('myProjects')) {
+    console.log("Initial populating Project Storage");
+    populateProjectStorage(myProjects, 'myProjects');
+}
+
+// Initial load and rendering of projects
+let projectIndex = 0;
+let projects = getProjects();
+
+projects.forEach((project) => {
+    addProjectHeader(project);
+});
+renderProjectPage(projects[projectIndex]);
+updateFormProjectOptions();
 
 const taskForm = document.querySelector('#task-form');
-
 taskForm.addEventListener('submit', function(event) {
     event.preventDefault();
 
-    // Get form values
     const title = document.querySelector('#title').value;
+    const description = document.querySelector('#description').value;
     const dueDate = document.querySelector('#due-date').value;
     const priority = document.querySelector('#priority').value;
-    const notes = document.querySelector('#notes').value;
+    const projectValue = parseInt(document.querySelector('#project').value);
+    const project = projects[projects.findIndex(project => project.projectId === projectValue)];
+    const projectId = project.projectId;
 
-    // Create new Task object with form values
-    const newTask = new Task(title, dueDate, priority, notes);
+    const newTask = new Task(title, description, dueDate, priority, projectId);
 
-    // Create the new task in the DOM
-    console.log(newTask.getTaskInfo());
+    addTask(newTask);
 
-    // Reset form values and close modal
+    if (projects[projectIndex] === project) {
+        createTaskCard(newTask);
+    }
+
     taskForm.reset();
 });
+
+// Handle completing tasks
+document.addEventListener('click', function(e) {
+    const button = e.target.closest('.completion-button');
+
+    if (button) {
+        let tasks = getTasks();
+        const taskId = parseInt(button.getAttribute('data-id'), 10);
+        const taskIndex = tasks.findIndex(task => task.taskId === taskId);
+        tasks[taskIndex].completion = 'complete';
+        populateTaskStorage(tasks, 'myTasks');
+        removeTaskCard(taskId);
+    }
+});
+
+// Handle project page navigation
+document.addEventListener('click', (e) => {
+    let projects = getProjects();
+    const project = e.target.closest('.project-header-container');
+
+    if (project) {
+        const projectId = parseInt(project.getAttribute('data-id'), 10);
+        const selectedProject = projects.findIndex(proj => proj.projectId === projectId);
+        renderProjectPage(projects[selectedProject]);
+        projectIndex = selectedProject;
+    }
+});
+
+const sidebar = document.querySelector('#sidebar-toggle');
+sidebar.addEventListener('click', () => {
+    hideSidebar();
+});
+
+export { }
--- src/task.js
+++ src/task.js
@@ -1,22 +1,39 @@
-// Task actions
-    // Create To Do
-    // Edit To Do
-    // Update To Completion Status
+import { myProjects, myTasks } from "./data.js";
+import { getTasks, getProjects, populateTaskStorage } from './storage.js';
 
-export class Task {
 
-    static COMPLETION_STATUSES = ['complete', 'incomplete'];
-    static PRIORITY_OPTIONS = ['low', 'medium', 'high'];
+function addTask(task) {
+    let tasks = getTasks();
 
-    constructor(title, dueDate, priority, notes, completion) {
-        this.title = title;
-        this.dueDate = dueDate;
-        this.priority = priority;
-        this.notes = notes;
-        completion = 'incomplete';
-    }
-    
-    getTaskInfo() {
-        return `${this.title}, due: ${this.dueDate}, priority: ${this.priority}, notes: ${this.notes}, completion status: ${this.completion}`
-    }
+    tasks.push(task);
+    populateTaskStorage(tasks, 'myTasks');
 }
+
+function editTask(title, description, dueDate, priority, projectId, taskId) {
+    let tasks = getTasks();
+    let projects = getProjects();
+    const currentTask = tasks[tasks.findIndex(task => task.taskId === taskId)];
+
+    currentTask.title = title;
+    currentTask.description = description;
+    currentTask.dueDate = dueDate;
+    currentTask.priority = priority;
+
+    const currentProjectId = parseInt(currentTask.projectId);
+  
+    if (parseInt(projectId) !== currentProjectId) {
+        const newProjectId = parseInt(projectId);
+        const newProject = projects.find(project => project.projectId === newProjectId);
+        if (!newProject) {
+            console.log("New project assignment not found");
+            return;
+        }
+        currentTask.projectId = parseInt(newProjectId);
+    }
+    else {
+        currentTask.projectId = currentProjectId;
+    }
+    populateTaskStorage(tasks, 'myTasks');
+}
+
+export { editTask, addTask }
